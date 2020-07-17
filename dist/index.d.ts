46d42692697e3b62fda5957733e84d6b16b0349a
Luiz Fellype <lfcassago@gmail.com>
2020-07-16 22:00:38 -0300

    doc: all the minified of the onPageChange event feature

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,8 +1,13 @@
 import * as React from "react";
+export interface OnPageChangeProps {
+    page: number;
+    totalPage: number;
+}
 interface Props {
     data: Array<any>;
     pageSize: number;
     renderItem: Function;
+    onPageChange: ({ page }: OnPageChangeProps) => any;
 }
 declare const Main: React.FC<Props>;
 export default Main;
